f64fadfbe8ed65070a9c3baa2e00f418ead4e920
kamal <kamalbajiya15@gmail.com>
2025-08-13 16:58:40 +0530

    auto-commit

--- data type.cpp
+++ data type.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main() {
-    // int a, b, c;
+// int a, b, c;
 // short sa=9;
 // cout<<sa;}
 
-int marksInMaths = 90;
-cout <<" The marks in Maths are: "<<marksInMaths;
+// int marksInMaths = 90;
+// cout <<" The marks in Maths are: "<<marksInMaths;
 
+short a;
+int b;
+long c;
+long long d;
+float const score = 43.33;
+cout<<"The score is "<<score;
 return 0;}
