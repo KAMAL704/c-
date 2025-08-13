84304249b495382f76a328b54f7e393dafad5aa5
kamal <kamalbajiya15@gmail.com>
2025-08-13 17:10:19 +0530

    auto-commit

---
+++ userinput.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int a, b, c;
+// short sa=9;
+// cout<<sa;}
+
+// int marksInMaths = 90;
+// cout <<" The marks in Maths are: "<<marksInMaths;
+
+// short a;
+// int b;
+// long c;
+// long long d;
+// float score = 43.33
+// cout<<"The score is "<<score;
+
+int a, b;
+cout<<"Enter first name ";
+cin>>a;
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
