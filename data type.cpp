8c47845cdf93c44a02f15e0214f87400cef7f29b
kamal <kamalbajiya15@gmail.com>
2025-08-13 16:45:41 +0530

    auto-commit

--- data type.cpp
+++ data type.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-// int main() {int a, b, c;
+int main() {
+    // int a, b, c;
 // short sa=9;
 // cout<<sa;}
 
 int marksInMaths = 90;
+cout <<" The marks in Maths are: "<<marksInMaths;
+
+return 0;}
