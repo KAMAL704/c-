3b8172eef006538401c17c71bb6037b4c0b7b75f
kamal <kamalbajiya15@gmail.com>
2025-09-25 21:02:28 +0530

    auto-commit

--- chapter1/c_basic.c
+++ chapter1/c_basic.c
@@ -1,18 +1,9 @@
 
 
-// #include<stdio.h>
-// int main(){
-//     printf("kamal");
-//     return 0;
-// }
-
-
 #include<stdio.h>
-int main() {
-int a=20;
-int b=40;
-int c=a+b;
-printf("%d",c);
-return 0;
+int main(){
+    printf("kamal );
+    return 0;
+}
 
-} 
+
