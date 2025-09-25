82da775653b661be5de330aac228c08abb6b8650
kamal <kamalbajiya15@gmail.com>
2025-09-25 21:04:33 +0530

    auto-commit

--- chapter1/c_basic.c
+++ chapter1/c_basic.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 int main(){
-    printf("myself kamal from Rajasthan. \nTrade GCS and Reg.No.254034051");
+    printf("yself kamal from Rajasthan. \nTrade GCS and Reg.No.254034051");
     return 0;
 }
 
