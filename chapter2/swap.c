c6a44125904bd3aba64241652b568b510157c663
kamal <kamalbajiya15@gmail.com>
2025-09-26 11:00:27 +0530

    auto-commit

--- chapter2/swap.c
+++ chapter2/swap.c
@@ -3,9 +3,9 @@ int main(){
     int a=2,b=6;
     int c;
     c=a+b;
-    a=c-b;
-    b=c-a;
-    printf("a=%d","b=%d",a,b);
+    a=c-a;
+    b=c-b;
+    printf("a=%d,b=%d",a,b);
     return 0;
 
     
