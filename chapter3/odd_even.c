d761d9a98c26aa0010702bd2b0645739cb5e80b0
kamal <kamalbajiya15@gmail.com>
2025-09-30 12:12:24 +0530

    auto-commit

--- chapter3/odd_even.c
+++ chapter3/odd_even.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main(){
-    int number:
-    char str:
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is even.\n", num);
+    else
+        printf("%d is odd.\n", num);
+
+    return 0;
 }
