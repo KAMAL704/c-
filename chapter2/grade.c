9d418f948d895adcc1da133a074f1b82f473b51d
kamal <kamalbajiya15@gmail.com>
2025-09-30 12:42:25 +0530

    auto-commit

--- chapter2/grade.c
+++ chapter2/grade.c
@@ -19,7 +19,7 @@ int main() {
         grade = 'D';
     else if (marks >= 0 && marks<=30)
         grade = 'F';
-        else if (marks >= 0 && marks<=30)
+        else if (marks >= 0 && marks<=3)
         grade = 'F';
     
 
