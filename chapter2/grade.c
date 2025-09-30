464978d1707b79f8c89997befeb8ea9822c762b0
kamal <kamalbajiya15@gmail.com>
2025-09-30 12:53:42 +0530

    auto-commit

--- chapter2/grade.c
+++ chapter2/grade.c
@@ -12,7 +12,7 @@ int main() {
     else if (marks >= 80 && marks <=90)
         grade = 'B';
     else if (marks >= 70 && marks <=80)
-        grade = 'B';
+        grade = 'C';
     else if (marks >= 60 && marks <=70)
         grade = 'C';
     else if (marks > 30 && marks <=60)
