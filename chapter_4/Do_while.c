34a3ef21617c7cff0cedf25c32e92b70a8dcac81
kamal <kamalbajiya15@gmail.com>
2025-10-03 10:24:58 +0530

    auto-commit

--- chapter_4/Do_while.c
+++ chapter_4/Do_while.c
@@ -1,37 +1,17 @@
-// #include<stdio.h>
-// int main(){
-//     int num;
-//     int sum = 0;
-
-// do{
-
-//     printf("enter the number: ");
-//     scanf("%d", &num);
-// if(num>0)
-// sum += num;
-
-// }
-// while(num>0);
-//     printf("sum of number= %d\n" ,sum);
-//     return 0;
-// } 
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
     int num;
     int sum = 0;
 
-    do {
-        printf("Enter the number: ");
-        scanf("%d", &num);
+do{
 
-        if (num > 0) {   // only add positive numbers
-            sum += num;
-        }
+    printf("enter the number: ");
+    scanf("%d", &num);
+    if(num>0)
+    sum += num;
 
-    } while (num > 0);   // loop until user enters 0 or a negative number
-
-    printf("Sum of numbers = %d\n", sum);
-
-    return 0;
 }
+while(num>0);
+    printf("sum of number= %d\n" ,sum);
+    return 0;
+} 
