f5c72a5c1aaa037d93ed85f3be0d6a49d7d7aabb
kamal <kamalbajiya15@gmail.com>
2025-10-03 10:55:57 +0530

    auto-commit

--- chapter_4/printnatural.c
+++ chapter_4/printnatural.c
@@ -2,15 +2,16 @@
 
 int main() {
     int num = 1;   
-    int sum = 0;
+    
 
     do {
-        sum += num;   
-        num ++;        
+        printf("%d", num);
+        num++;
     } 
     while (num <= 10);   
 
-    printf("Sum of numbers from 1 to 10 = %d\n", sum);
+    
 
     return 0;
 }
+
