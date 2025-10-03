b9f824489eab7c8b0a827289b0d7f8ba88a7a9f3
kamal <kamalbajiya15@gmail.com>
2025-10-03 10:19:39 +0530

    auto-commit

--- chapter_4/Do_while.c
+++ chapter_4/Do_while.c
@@ -4,12 +4,14 @@ int main(){
     int sum = 0;
 
 do{
+    
     printf("enter the number: ");
     scanf("%d", &num);
 if(num>0);
 sum += num;
+
 }
-while(num>0); 
+while(sum>0); 
     printf("sum of number= %d\n" ,sum);
     return 0;
 } 
