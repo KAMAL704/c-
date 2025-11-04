db1158eee8337991766e2dda31f6f6247bcd030a
kamal <kamalbajiya15@gmail.com>
2025-11-04 12:15:05 +0530

    auto-commit

--- chapter5/calculator.c
+++ chapter5/calculator.c
@@ -1,30 +1,30 @@
-// #include<stdio.h>
-// int main(){
-//     int a =10;
-//     int b = 4;
-//     int c ;
-//     printf("enter the case number");
-//     scanf("%d", &c);
-//     switch(c){
-//         case 1:
-//         printf("%d",a+b);
-//         break;
+#include<stdio.h>
+int main(){
+    int a =10;
+    int b = 4;
+    int c ;
+    printf("enter the case number");
+    scanf("%d", &c);
+    switch(c){
+        case 1:
+        printf("%d",a+b);
+        break;
 
-//         case 2:
-//         printf("%d",a-b);
-//         break;
+        case 2:
+        printf("%d",a-b);
+        break;
 
-//         case 3:
-//         printf("%d",a*b);
-//         break;
+        case 3:
+        printf("%d",a*b);
+        break;
 
-//         case 4:
-//         printf("%d",a/b);
-//         break;
+        case 4:
+        printf("%d",a/b);
+        break;
 
-//     }
-//     return 0;
-// }
+    }
+    return 0;
+}
 
 
 
