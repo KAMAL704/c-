67c82b8bd33f1a82ba785f49ef4a8d067f5e435c
kamal <kamalbajiya15@gmail.com>
2025-11-04 12:36:48 +0530

    auto-commit

---
+++ chapter3/prime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1, n)
+        printf("Not Prime");
+    else if (n == 2)
+        printf("Prime");
+    else if (n % 2 == 0)
+        printf("Not Prime");
+    else if (n % 3 == 0 && n != 3)
+        printf("Not Prime");
+    else if (n % 5 == 0 && n != 5)
+        printf("Not Prime");
+    else if (n % 7 == 0 && n != 7)
+        printf("Not Prime");
+    else
+        printf("Prime");
+
+    return 0;
+}
