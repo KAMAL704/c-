48bb1ac0fa8b68efb3ccfd58b22237ef2053979a
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:09:15 +0530

    auto-commit

--- chapter6/array.c
+++ chapter6/array.c
@@ -1,8 +1,17 @@
+// #include<stdio.h>
+// int main(){
+//     int a [10] = {1,4,7,5,9,42,56,11,55,353};
+//     printf("%d", a[3]);
+//     return 0;
+// }
+
+
+
+
 #include<stdio.h>
 int main(){
-    int a [5] = {1,4,7,5,9}
-    printf("%d", )
-
-
-
+    int a [10] = {1,4,7,5,9,42,56,11,55,353};
+    for{}
+    printf("%d", a[3]);
+    return 0;
 }
