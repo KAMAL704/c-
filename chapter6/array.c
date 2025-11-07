c5f4d5512b4cc645c88b0c3cd811e2750708741e
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:21:41 +0530

    auto-commit

--- chapter6/array.c
+++ chapter6/array.c
@@ -13,7 +13,7 @@ int main(){
     int j [10] = {1,4,7,5,9,42,56,11,55,35};
     for(int i = 0; i<=10; i++)
     {
-    printf("value of i:,%d\n" , i);
+    printf("value of i:,%d\n" , i ,j[i]);
 }
     return 0;
 }
