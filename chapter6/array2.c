ac58b1ea87594fdc34e1fed81acd17550c5df89d
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:39:24 +0530

    auto-commit

--- chapter6/array2.c
+++ chapter6/array2.c
@@ -1,9 +1,21 @@
+// #include<stdio.h>
+// int main(){
+//     char j [10] = {'k','d','r','e','y','i','f','f','v','n'};
+//     for(int i = 0; i<10; i++)
+//     {
+//     printf("%c\n", j[i]);
+
+// }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
+    int k [10] = {1,4,7,5,9,42,56,11,55,35};
     char j [10] = {'k','d','r','e','y','i','f','f','v','n'};
     for(int i = 0; i<10; i++)
     {
-    printf("%c\n", j[i]);
+    printf("%c\n" "%d\n", j[i] ,k[i]);
 
 }
     return 0;
