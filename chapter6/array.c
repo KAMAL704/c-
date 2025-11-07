fa2e5142c24d84d9916ffcbaa5473a15c69ba58f
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:25:03 +0530

    auto-commit

--- chapter6/array.c
+++ chapter6/array.c
@@ -11,9 +11,10 @@
 #include<stdio.h>
 int main(){
     int j [10] = {1,4,7,5,9,42,56,11,55,35};
-    for(int i = 0; i<=10; i++)
+    for(int i = 0; i<10; i++)
     {
-    printf("value of i:,%d\n" , i ,j[i]);
+    printf("value of i: %d %d\n", i, j[i]);
+
 }
     return 0;
 }
