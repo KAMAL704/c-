01857e921f2acb78efa8a8de6182f5fdeffd7fd3
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:46:45 +0530

    auto-commit

---
+++ chapter6/array2d.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    
+    char j [10] = {'k','d','r','e','y','i','f','f','v','n'};
+    for(int i = 0; i<10; i++)
+    {
+    printf("%c\n" "%d\n", j[i] ,k[i]);
+
+}
+    return 0;
+}
