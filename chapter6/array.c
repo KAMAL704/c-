2038a3cc7456ac624ea92a3e09831a9a6299984e
kamal <kamalbajiya15@gmail.com>
2025-11-07 10:14:17 +0530

    auto-commit

--- chapter6/array.c
+++ chapter6/array.c
@@ -10,8 +10,10 @@
 
 #include<stdio.h>
 int main(){
-    int a [10] = {1,4,7,5,9,42,56,11,55,353};
-    for{}
-    printf("%d", a[3]);
+    int i [10] = {1,4,7,5,9,42,56,11,55,35};
+    for(int i = 0; i<=10; i++)
+    {
+    printf("value of i","%d\n" , i);
+}
     return 0;
 }
