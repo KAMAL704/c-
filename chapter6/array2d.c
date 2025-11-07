14f16ef8e52720e5b70e3dbe9e002acfe7edeb68
kamal <kamalbajiya15@gmail.com>
2025-11-07 11:03:20 +0530

    auto-commit

--- chapter6/array2d.c
+++ chapter6/array2d.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
     
-    int k [3][3]  = {{1,2,3}
-                    {4,5,6}
+    int k [3][3]  = {{1,2,3},
+                    {4,5,6},
                     {7,8,9}};
 
     
     
-    printf("%d", k[3][3]);
+    printf("%d\n", k[2][3]);
     return 0;
 }
